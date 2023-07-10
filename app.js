063a7b79b879374cd72ebffe07a58b0bccd4ba62
Derek <derekdanielson84@gmail.com>
2023-07-10 10:40:31 -0600

    Notes added

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', './movies.json');
+
+/*
+readyState has 5 possible values
+
+0 - request not initialized
+1 - server connection established
+2 - request received
+3 - processing request
+4 - request finished and response is ready
+*/
+
+xhr.onreadystatechange = function (){
+
+}
