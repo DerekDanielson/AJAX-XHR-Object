5dd2bbffcbe2c662df79f9d2a054df1396a065fb
Derek <derekdanielson84@gmail.com>
2023-07-10 10:57:00 -0600

    Notes added

--- app.js
+++ app.js
@@ -13,5 +13,18 @@ readyState has 5 possible values
 */
 
 xhr.onreadystatechange = function (){
+    if (this.readyState === 4 && this.status === 200){
+     //console.log(JSON.parse(this.responseText));   
+        const data = JSON.parse(this.responseText);
 
+        data.forEach(movie => {
+            const li = document.createElement('li');
+            li.innerHTML = `<strong>${movie.title}</strong> - ${movie.year}`;
+            document.querySelector('#results').appendChild(li);
+        });
+    }
+    console.log(this.readyState);
+    console.log(this.status);
 }
+
+xhr.send();
